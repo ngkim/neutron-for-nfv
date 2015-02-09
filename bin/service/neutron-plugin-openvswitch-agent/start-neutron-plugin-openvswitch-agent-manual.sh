28b8e4dddadcf315cf7ec1526e794d19399dac53
Namgon Kim <day10000@gmail.com>
2015-02-09 12:37:29 +0900

    Add scripts for neutron-openvswitch-agent

---
+++ bin/service/neutron-plugin-openvswitch-agent/start-neutron-plugin-openvswitch-agent-manual.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+/usr/bin/python /usr/bin/neutron-openvswitch-agent --config-file=/etc/neutron/neutron.conf --config-file=/etc/neutron/plugins/ml2/ml2_conf.ini --log-file=/var/log/neutron/openvswitch-agent.log
+
+
